2a2dab22eca43ce23d46def847067d4845c546a8
Caleb Figgers <cdfiggers@gmail.com>
2022-02-24 12:41:42 -0600

    Defined baristas, gen-barista function

---
+++ src/coffee_shop/barista.clj
@@ -0,0 +1,30 @@
+(ns coffee-shop.barista
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def :barista/stat (s/and int?
+                            #(< % 6)
+                            #(> % 0)))
+(s/def :barista/name string?)
+(s/def :barista/skill :barista/stat)
+(s/def :barista/speed :barista/stat)
+(s/def :barista/barista (s/keys :req [:barista/name
+                                      :barista/skill
+                                      :barista/speed]))
+
+(def barista-names
+  ["Sonnie" "Tymothy" "Sybil" "Porty" "Coriss" "Ileane" "Brandea" "Claudius" "Elbertine" "Rickey"
+   "Arny" "Ardelle" "Diann" "Erik" "Margie" "Doe" "Mohammed" "Dory" "Dougy" "Raleigh" "Trevar"
+   "Isidor" "Maible" "Rachelle" "Selinda" "Griffy" "Hewett" "Tulley" "Kellen" "Fin" "Charmane" "Lars"
+   "Cosimo" "Any" "Goober" "Sayers" "Dugald" "Katrinka" "Dodie" "Claudie" "Fleurette" "Matias" "Filippo"
+   "Nonna" "Shermie" "Steffie" "Boycey" "Lynnette" "Chrysa" "Wendel" "Ericha" "Clay" "Pennie" "Gilbert"
+   "Sylvan" "Rex" "Dev" "Lonee" "Joe" "Boot" "Fenelia" "Thorpe" "Rozamond" "Josephina" "Susette"
+   "Steffen" "Egor" "Reggis" "Ruddie" "Gussie" "Colman" "Edmund" "Neile" "Danya" "Charleen" "Gus"
+   "Shannah" "Vanna" "Cullie" "Lina" "Zach" "Fredek" "Irma" "Regina" "Ivar" "Cookie" "Chas"
+   "Elijah" "Katya" "Lila" "Klarrisa" "Meta" "Nancie" "Dorry" "Denice" "Elle" "Gavan" "Kevyn" "Terry"
+   "Godfrey"])
+
+(defn gen-barista []
+  {:barista/name (barista-names (dec (rand-int 100)))
+   :barista/skill (inc (rand-int 5))
+   :barista/speed (inc (rand-int 5))})
+
