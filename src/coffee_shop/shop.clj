1a16b0ce4cbb2a7437a0ada12e9cf78ca6cc3e4f
Caleb Figgers <cdfiggers@gmail.com>
2022-02-24 20:57:26 -0600

    Created shop.clj

---
+++ src/coffee_shop/shop.clj
@@ -0,0 +1,34 @@
+(ns coffee-shop.shop
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def :equip/quality (s/and int?
+                            #(< 0 %)
+                            #(> 11 %)))
+(s/def :equip/name string?)
+(s/def :equip/type #{:equip/espresso-machine})
+(s/def :shop/equipment (s/keys :req [:equip/type
+                                     :equip/name
+                                     :equip/quality]))
+(s/def :shop/equipments (s/coll-of :shop/equipment))
+
+(def old-espresso-machine 
+  {:equip/type :equip/espresso-machine
+   :equip/name "Old Espresso Machine"
+   :equip/quality 5})
+
+;; (s/valid? :shop/equipment old-espresso-machine) => true
+
+(s/def :shop/employees (s/map-of string? :barista/barista))
+(s/def :shop/ambiance (s/and int?
+                            #(< 0 %)
+                            #(> 11 %)))
+(s/def :shop/shop (s/keys :req [:shop/employees
+                                :shop/equipments
+                                :shop/ambiance]))
+
+(def starting-shop 
+  {:shop/employees {"Ardelle" #:barista{:name "Ardelle", :skill 3, :speed 1, :accuracy 3}}
+   :shop/equipments [old-espresso-machine]
+   :shop/ambiance 5})
+
+;; (s/valid? :shop/shop starting-shop) => true
