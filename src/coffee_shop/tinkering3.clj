fa6f631b7305d942092f6990430f809a85fcba2f
Caleb Figgers <cdfiggers@gmail.com>
2022-02-24 21:23:26 -0600

    Commented out

--- src/coffee_shop/tinkering3.clj
+++ src/coffee_shop/tinkering3.clj
@@ -1,8 +1,6 @@
 (ns coffee-shop.tinkering3)
 
-(comment
-  (defn coffee-shop-2 [days]
-    (into (sorted-map)
-          (map #(vector % (run-day))
-               (range 1 (inc days)))))
-  )
+;; (defn coffee-shop-2 [days]
+;;   (into (sorted-map)
+;;         (map #(vector % (run-day))
+;;              (range 1 (inc days)))))
