b879a78d393084ead416b32a64b5d7f665f272e5
Caleb Figgers <cdfiggers@gmail.com>
2022-03-03 19:50:29 -0600

    Testing gen-order

--- test/coffee_shop/customer_test.clj
+++ test/coffee_shop/customer_test.clj
@@ -1,7 +1,15 @@
 (ns coffee-shop.customer-test
   (:require [clojure.test :refer :all]
-            [coffee-shop.customer :refer :all]))
+            [coffee-shop.customer :refer :all]
+            [clojure.test.check.clojure-test :refer :all]
+            [clojure.spec.alpha :as s]
+            [clojure.test.check.properties :as prop]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest customer-test
+  (testing "Testing gen-order"
+    (testing "should match spec :customer/order"
+      #_{:clj-kondo/ignore [:unresolved-symbol]}
+      (defspec test-spec-gen-order
+        100
+        (prop/for-all []
+                      (s/valid? :customer/order (gen-order)))))))
