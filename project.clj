5a3e1c2c6fcd54a0f077bec9465e18c8d5ed03e6
Caleb Figgers <cdfiggers@gmail.com>
2022-03-03 19:23:01 -0600

    Testing

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.rpl/specter "1.1.3"]]
+                 [com.rpl/specter "1.1.3"]
+                 [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot coffee-shop.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/coffee_shop/barista_test.clj
+++ test/coffee_shop/barista_test.clj
@@ -3,5 +3,6 @@
             [coffee-shop.barista :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Testing gen-barista"
+    (testing "should match spec :barista/barista"
+      (is (= 0 1)))))
