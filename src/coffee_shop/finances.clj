b587f8efb8aec78729b6a99f66a377bfab0aadc5
Caleb Figgers <cdfiggers@gmail.com>
2022-03-19 22:06:18 -0500

    Add TODO(#17)

--- src/coffee_shop/finances.clj
+++ src/coffee_shop/finances.clj
@@ -1,13 +1,14 @@
 (ns coffee-shop.finances
   (:require [clojure.spec.alpha :as s]))
 
-;; TODO - A function for determining the monetary impact of a day
-;; - TODO - A function for determining the monegary impact of a shift
-;;   - TODO - A function for determining the monetary impact of each 
-;;            order/customer
-;;     - TODO - A function for determining the monetary impact of serving
-;;       each drink
-;;   - TODO - A function for determining the monetary impact of each
-;;     barista
+;; TODO(#17): A function for determining the monetary impact of a day
 
-;; TODO - A function for determining the monetary impact of rent
+  ;; TODO: A function for determining the monetary impact of a shift
+
+    ;; TODO: A function for determining the monetary impact of each order/customer
+
+    ;; TODO: A function for determining the monetary impact of serving each drink
+
+  ;; TODO: A function for determining the monetary impact of each barista
+
+;; TODO: A function for determining the monetary impact of rent
